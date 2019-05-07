8c7b01fe33d2906ae9f3574060d64bb10c51e8be
matucasal <matias.casal@yahoo.com.ar>
2019-05-07 12:57:28 -0300

    "Subo todo l oque vimos hoy"

--- CursoVueJs-Clase1/js/main.js
+++ CursoVueJs-Clase1/js/main.js
@@ -6,7 +6,30 @@ const app = new Vue(
         data: {
             mensaje: 'Hola VueJS',
             mensajeHTML: '<p style= "color:red;">Hola VueJS</p>',
-            mostrar: false
+            mostrar: false,
+            //usuarios: ['Pedro','Juan','Ana','Laura']
+            //ahora array de objetos
+            usuarios: 
+            [
+                {nombre:'Pedro', apellido:'Gomez', curso:false, edad: 32, foto:'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-512.png'},
+                {nombre:'Mariano', apellido:'Perez', curso:true, edad: 21, foto:'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png'},
+                {nombre:'Matias', apellido:'Martinez', curso:false, edad: 29, foto:'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/1_avatar-512.png'},
+                {nombre:'Sofia', apellido:'Lopez', curso:true, edad: 21, foto:'https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-512.png'},
+            ]
+        },
+        methods: {
+            //Diferentes formas de llamar metodos
+            cambiarVistaTexto1 : function () {
+                this.mostrar=!this.mostrar
+            },
+
+            borrarUsuario : function (index) {
+                //Saca un elemento del array (splice (indice, cuantos))
+                this.usuarios.splice(index,1);
+            },
+
+            cambiarVistaTexto2() {
+            }
         }
     }
 )
